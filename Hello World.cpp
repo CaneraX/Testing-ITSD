48f90b9c16278f9bc81cad91ed7807f65b108026
LAPTOP-NC958D2A\ammar <ammarxzarif@gmail.com>
2021-02-06 12:29:14 +0800

    no message

--- Hello World.cpp
+++ Hello World.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 int main(){
 	  char Name;
+	  double Age;
 		cout<<"Hello World"<<endl;
 		cout<<"Enter your Name"<<endl;
 		cin >>  Name;
+		cout<<"Enter your Age"<<endl;
+		cin >> Age;
 	return 0;
 
 }
